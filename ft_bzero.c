ece306a246a7e3bb403899a9fee6d3c1ad8d9c7a
David Agredano Granell <dagredan@student.42barcelona.com>
2024-12-19 16:15:32 +0100

    Add ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/18 11:50:07 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/19 16:12:28 by dagredan         ###   ########.fr       */
+/*   Created: 2024/12/19 16:08:09 by dagredan          #+#    #+#             */
+/*   Updated: 2024/12/19 16:12:58 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <stddef.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-void	*ft_memset(void *s, int c, size_t n);
-
-#endif
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
