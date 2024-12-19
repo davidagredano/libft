7086c60b8b9b0c334d485d896697629469dabf68
David Agredano Granell <dagredan@student.42barcelona.com>
2024-12-19 15:59:33 +0100

    Add ft_memset

---
+++ ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 14:40:54 by dagredan          #+#    #+#             */
+/*   Updated: 2024/12/19 15:49:12 by dagredan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
+	unsigned char *s_unsigned_char;
+
+	s_unsigned_char = s;
+	i = 0;
+	while (i < n)
+	{
+		s_unsigned_char[i] = c;
+		i++;
+	}
+	return (s);
+}
