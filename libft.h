0417b8e2a55a1bba229b4a56f22e4b91e72ab1ea
David Agredano <davidagredano@gmail.com>
2024-12-19 20:54:40 +0100

    Update header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 11:50:07 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/19 16:12:28 by dagredan         ###   ########.fr       */
+/*   Updated: 2024/12/19 20:52:37 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
+//int ft_isalnum(int c);
+//int ft_isascii(int c);
+//int ft_isprint(int c);
+size_t ft_strlen(const char *s); 
+
 void	*ft_memset(void *s, int c, size_t n);
+//void	ft_bzero(void *s, size_t n);
 
 #endif
