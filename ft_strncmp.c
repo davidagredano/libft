041591ec0e450fd441b4170d929cbfebabb0da5c
David Agredano <davidagredano@gmail.com>
2024-12-21 21:50:54 +0100

    Add ft_strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 20:58:44 by dagredan          #+#    #+#             */
+/*   Updated: 2024/12/21 21:47:16 by dagredan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (i < n - 1 && s1[i] == s2[i] && s1[i] != '\0')
+		i++;
+	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 11:50:07 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/21 19:16:09 by dagredan         ###   ########.fr       */
+/*   Updated: 2024/12/21 20:59:21 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ void	*ft_memset(void *s, int c, size_t n);
 //int	ft_tolower(int c);
 //char	*ft_strchr(const char *s, int c);
 //char	*ft_strrchr(const char *s, int c);
+//int	strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
