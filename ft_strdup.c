3e27eff348a68c315189d1c0b8f123761f7231d4
David Agredano Granell <dagredan@student.42barcelona.com>
2024-12-23 17:21:30 +0100

    Add ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/23 16:45:23 by dagredan          #+#    #+#             */
+/*   Updated: 2024/12/23 17:11:20 by dagredan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	size_t	size;
+
+	size = ft_strlen(s) + 1;
+	dup = malloc(size);
+	if (!dup)
+		return (NULL);
+	ft_strlcpy(dup, s, size);
+	return (dup);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 11:50:07 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/23 13:25:07 by dagredan         ###   ########.fr       */
+/*   Updated: 2024/12/23 17:11:42 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	*ft_memset(void *s, int c, size_t n);
 //void	ft_bzero(void *s, size_t n);
 //void	*ft_memcpy(void *dest, const void *src, size_t n);
 //void	*ft_memmove(void *dest, const void *src, size_t n);
-//size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 //size_t	ft_strlcat(char *dst, const char *src, size_t size);
 //int	ft_toupper(int c);
 //int	ft_tolower(int c);
@@ -36,7 +36,8 @@ void	*ft_memset(void *s, int c, size_t n);
 //void	*ft_memchr(const void *s, int c, size_t n);
 //int	ft_memcmp(const void *s1, const void *s2, size_t n);
 //char	*ft_strnstr(const char *big, const char *little, size_t len);
-//int	atoi(const char *nptr);
-//void	*calloc(size_t nmemb, size_t size);
+//int	ft_atoi(const char *nptr);
+//void	*ft_calloc(size_t nmemb, size_t size);
+//char	*ft_strdup(const char *s);
 
 #endif
