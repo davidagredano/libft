80348a5b246e29361f5c87b07590e70688b76c6a
David Agredano <davidagredano@gmail.com>
2024-12-27 13:58:51 +0100

    Update ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 18:09:52 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/21 23:13:22 by dagredan         ###   ########.fr       */
+/*   Updated: 2024/12/27 13:54:32 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		if (s[i] == (char) c)
 			return ((char *) &s[i]);
 		i++;
 	}
-	if (c == '\0')
+	if (s[i] == '\0' && (char) c == '\0')
 		return ((char *) &s[i]);
 	return (NULL);
 }
