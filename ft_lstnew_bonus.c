15b7bbe0dfa6d94d6a7a1044d939beb7f32ce678
David Agredano <davidagredano@gmail.com>
2024-12-27 21:43:07 +0100

    Update ft_lstnew to protect the malloc

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 19:14:58 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/27 19:22:48 by dagredan         ###   ########.fr       */
+/*   Updated: 2024/12/27 21:42:10 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*node;
 
 	node = ft_calloc(1, sizeof(t_list));
+	if (!node)
+		return (NULL);
 	node->content = content;
 	node->next = NULL;
 	return (node);
