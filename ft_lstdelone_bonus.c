f442ce29392d0cea97ad5ed98e18a2f2f5b2d1e5
David Agredano <davidagredano@gmail.com>
2024-12-28 11:29:06 +0100

    Add ft_lstdelone

---
+++ ft_lstdelone_bonus.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/28 10:30:18 by dagredan          #+#    #+#             */
+/*   Updated: 2024/12/28 11:23:27 by dagredan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft_bonus.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	if (lst == NULL || del == NULL)
+		return ;
+	del(lst->content);
+	free(lst);
+}
