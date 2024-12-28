5f2ca88a9633462e7c817249c864d9b4b9fbfeb1
David Agredano <davidagredano@gmail.com>
2024-12-28 17:05:23 +0100

    Add ft_lstmap

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/28 15:59:30 by dagredan          #+#    #+#             */
+/*   Updated: 2024/12/28 17:02:18 by dagredan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft_bonus.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_lst;
+	t_list	*new_node;
+	void	*new_content;
+
+	if (f == NULL || del == NULL)
+		return (NULL);
+	new_lst = NULL;
+	while (lst != NULL)
+	{
+		new_content = f(lst->content);
+		if (new_content == NULL)
+			break ;
+		new_node = ft_lstnew(new_content);
+		if (new_node == NULL)
+			break ;
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
+	}
+	if (lst != NULL)
+	{
+		free(new_content);
+		ft_lstclear(&new_lst, del);
+		return (NULL);
+	}
+	return (new_lst);
+}
--- libft_bonus.h
+++ libft_bonus.h
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 11:50:07 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/28 13:15:49 by dagredan         ###   ########.fr       */
+/*   Updated: 2024/12/28 16:37:12 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,13 +58,14 @@ typedef struct		s_list
 	struct s_list	*next;
 }					t_list;
 
-//t_list	*ft_lstnew(void *content);
+t_list	*ft_lstnew(void *content);
 //void	ft_lstadd_front(t_list **lst, t_list *new);
-//int		ft_lstsize(t_list *lst);
+//int	ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
-//void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
-//void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 //void	ft_lstiter(t_list *lst, void (*f)(void *));
+//t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
