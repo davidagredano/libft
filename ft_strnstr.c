b3e5058cb9cf6c07d20e040efa60e08210b832a9
David Agredano <davidagredano@gmail.com>
2024-12-30 13:01:19 +0100

    Fix ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 10:22:23 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/23 12:00:35 by dagredan         ###   ########.fr       */
+/*   Updated: 2024/12/30 13:00:49 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	while (i < len && big[i] != '\0')
 	{
 		j = 0;
-		while (little[j] == big[i + j] && i + j < len)
+		while (little[j] != '\0' && little[j] == big[i + j] && i + j < len)
 			j++;
 		if (little[j] == '\0')
 			return ((char *) &big[i]);
