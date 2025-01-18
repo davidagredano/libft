7846d371340d7cccae4f7eb049f1e723335cd39f
David Agredano <davidagredano@gmail.com>
2025-01-18 19:15:28 +0100

    Update ft_putstr to return -1 on error

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 09:33:53 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/13 15:52:59 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/18 18:51:30 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 /**
  * Writes a string to the standard output, 
  * without its terminating null byte ('\0').
- * Return the number of characters printed.
+ * Return the number of characters printed, or -1 on error.
  */
 
 int	ft_putstr(const char *s)
 {
 	if (!s)
-		return (0);
+		return (-1);
 	return ((int) write(STDOUT_FILENO, s, ft_strlen(s)));
 }
